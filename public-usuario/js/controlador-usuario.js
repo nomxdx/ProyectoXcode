8e1e48165730a3647c6c6ba61c9cb9b8f31bda8e
Ronald Fúnez <favier_funez94@yahoo.com>
2018-12-09 23:44:58 -0600

    exposicion de carpetas

---
+++ public-usuario/js/controlador-usuario.js
@@ -0,0 +1,23 @@
+
+function MouseIn(){
+	document.getElementById("carpeta").style.borderRadius = "100";
+	document.getElementById("carpeta").style.backgroundColor = "lightblue";
+
+};
+
+function MouseOut(){
+	document.getElementById("carpeta").style.backgroundColor = "white";	
+
+};
+
+function alertas(){
+	alert("Hello! Me diste click");
+};
+
+function MouseIn1(){
+	document.getElementById("proyecto").style.backgroundColor = "lightblue";
+};
+
+function MouseOut1(){
+	document.getElementById("proyecto").style.backgroundColor = "white";	
+};
